4005aa127df6c68f8279bd09ce53fa93f34b8c50
svper563 <btteachey@gmail.com>
2023-04-09 21:45:23 -0400

    Added contact window

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('page loaded.')
-})
+});
 
 const contactLink = document.querySelector('.link');
+const contactContainer = document.querySelector('#contact-container');
+const contactClose = document.querySelector('#contact-close-button');
 
 contactLink.addEventListener('click', () => {
-    console.log('Link clicked.')
-})
+    contactContainer.style.opacity = 1;
+    contactContainer.style.width = '90vw';
+    contactContainer.style.height = '90vh';
+    console.log('Link clicked.');
+
+});
+
+contactClose.addEventListener('click', () => {
+    contactContainer.style.opacity = 0;
+    contactContainer.style.width = '0vw';
+    contactContainer.style.height = '0vh';
+    console.log('close button clicked.')
+});
